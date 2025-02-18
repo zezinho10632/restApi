c8d9c37ac30a8e51de8c906a3fdede374c40f7cf
José <jn10632@gmail.com>
2025-02-18 16:49:13 -0300

    estrutura inicial

---
+++ src/main/java/com/restApi/restApi/Greeting.java
@@ -0,0 +1,20 @@
+package com.restApi.restApi;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
---
+++ src/main/java/com/restApi/restApi/GreetingController.java
@@ -0,0 +1,18 @@
+package com.restApi.restApi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+    private static final String template = "Hello, %s!";  //cria metodo de template
+    private static final AtomicLong counter = new AtomicLong();  //cria classe para uso de counter
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "Word") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
